d9a3700ee38121469c2a9f0a5fe19b87925d2e14
amritsiidhu <amritsidhu360@gmail.com>
2019-06-16 17:28:57 +0100

    Implement contact form

--- app/main/views/index.py
+++ app/main/views/index.py
@@ -1,10 +1,11 @@
 from datetime import datetime
-from flask import render_template
+from flask import current_app, render_template, redirect, url_for
 from random import randint
 from app.main import main
 from app import api_client
 from app.main.decorators import setup_subscription_form
 from six.moves.html_parser import HTMLParser
+from app.main.forms import ContactForm
 
 
 @main.route('/', methods=['GET', 'POST'])
@@ -31,12 +32,32 @@ def index(**kwargs):
                 event['past'] = True
                 all_events.append(event)
 
+    contact_form = ContactForm()
+    if contact_form.validate_on_submit():
+        try:
+            api_client.add_contact_info(contact_form.name.data)
+            return redirect(url_for('.index'))
+        except Exception as e:
+            print(e)
+            return render_template(
+                'views/home.html',
+                images_url=current_app.config['IMAGES_URL'],
+                main_article=articles[index],
+                articles=articles,
+                events=events,
+                current_page='',
+                contact_form=contact_form,
+                error=e,
+                **kwargs
+            )
+
     return render_template(
         'views/home.html',
         main_article=main_article,
         articles=articles,
         all_events=all_events,
         current_page='',
+        contact_form=contact_form,
         **kwargs
     )
 
@@ -44,9 +65,18 @@ def index(**kwargs):
 @main.route('/about')
 @setup_subscription_form
 def about(**kwargs):
+    contact_form = ContactForm()
+    events = api_client.get_events_in_future(approved_only=True)
+    for event in events:
+        if event['event_type'] == 'Introductory Course':
+            event['carousel_text'] = 'Courses starting {}'.format(event['event_monthyear'])
+
+    articles = api_client.get_articles_summary()
+    index = randint(0, len(articles) - 1)
     return render_template(
         'views/about.html',
         current_page='about',
+        contact_form=contact_form,
         **kwargs
     )
 
@@ -54,9 +84,11 @@ def about(**kwargs):
 @main.route('/resources')
 @setup_subscription_form
 def resources(**kwargs):
+    contact_form = ContactForm()
     return render_template(
         'views/resources.html',
         current_page='resources',
+        contact_form=contact_form,
         **kwargs
     )
 
@@ -76,6 +108,8 @@ def whats_on(**kwargs):
             event = all_past_events.pop(-1)
             past_events.append(event)
 
+    contact_form = ContactForm()
+
     return render_template(
         'views/whats_on.html',
         current_page='whats-on',
@@ -83,6 +117,7 @@ def whats_on(**kwargs):
         articles=articles,
         future_events=future_events,
         past_events=past_events,
+        contact_form=contact_form,
         **kwargs
     )
 
@@ -90,9 +125,11 @@ def whats_on(**kwargs):
 @main.route('/what-we-offer')
 @setup_subscription_form
 def what_we_offer(**kwargs):
+    contact_form = ContactForm()
     return render_template(
         'views/what_we_offer.html',
         current_page='what-we-offer',
+        contact_form=contact_form,
         **kwargs
     )
 
@@ -100,9 +137,11 @@ def what_we_offer(**kwargs):
 @main.route('/e-shop')
 @setup_subscription_form
 def e_shop(**kwargs):
+    contact_form = ContactForm()
     return render_template(
         'views/e-shop.html',
         current_page='e-shop',
+        contact_form=contact_form,
         **kwargs
     )
 
