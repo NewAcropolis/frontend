ca11da0ae71395d029a15d26b9548110c1c50386
kentsang.gds <ken.tsang@digital.cabinet-office.gov.uk>
2021-10-18 14:14:17 +0100

    Update main to use app rather than application

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from flask_script import Manager, Server
 from app import create_app
 
 
-application = create_app()
+app = create_app()
 
-manager = Manager(application)
+manager = Manager(app)
 manager.add_command("runserver", Server(host='0.0.0.0'))
 
 
-@application.template_filter('strfdate')
+@app.template_filter('strfdate')
 def _jinja2_filter_datetime(date, fmt=None):
     date_obj = datetime.strptime(date, '%Y-%m-%d')
     return date_obj.strftime('%A %-d %B')
@@ -18,7 +18,7 @@ def _jinja2_filter_datetime(date, fmt=None):
 @manager.command
 def list_routes():
     """List URLs of all application routes."""
-    for rule in sorted(application.url_map.iter_rules(), key=lambda r: r.rule):
+    for rule in sorted(app.url_map.iter_rules(), key=lambda r: r.rule):
         print("{:10} {}".format(", ".join(rule.methods - set(['OPTIONS', 'HEAD'])), rule.rule))
 
 
