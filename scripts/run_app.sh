0062982334b5a93a513dd0154bf8f8261a7e1593
kentsang.gds <kenlt.uk@gmail.com>
2021-10-19 01:18:17 +0100

    Update run_app.sh

--- scripts/run_app.sh
+++ scripts/run_app.sh
@@ -1,28 +1,7 @@
 #!/bin/bash
 set +e
 
-echo "Deprecated -- use make dev-server instead"
 
-ENV=development
-www_dir="www-$ENV"
+port=8080
 
-port=5050
-
-if [ ! -z "$1" ]; then
-    www_dir="www-$1"
-    cd $www_dir
-    port="$(python app/config.py -e $1)"
-
-    ENV=$1
-fi
-
-# kill any existing services running on port
-# fuser -k -n tcp $port
-
-echo "hosting on $www_dir"
-
-python main.py runserver --port $port
-
-if [ $www_dir != "www" ]; then
-    exit 0
-fi
+python3 main.py runserver --port $port
