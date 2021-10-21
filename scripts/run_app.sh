04b9e291b883ec0a544628d06428977ec8688c61
kentsang.gds <kenlt.uk@gmail.com>
2021-10-21 01:51:34 +0100

    Update run_app to check for venv

--- scripts/run_app.sh
+++ scripts/run_app.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set +e
 
-
 port=8080
+
+if [ -z "$VIRTUAL_ENV" ] && [ -d venv ]; then
+  echo 'activate venv'
+  source ./venv/bin/activate
+fi
 
 python3 main.py runserver --port $port
