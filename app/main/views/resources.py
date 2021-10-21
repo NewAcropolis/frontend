a1da4d58f29c37fabb4b4abdb298c471654b45ef
kentsang.gds <kenlt.uk@gmail.com>
2021-10-21 11:07:29 +0100

    Only get books from API if not showing maint msg

--- app/main/views/resources.py
+++ app/main/views/resources.py
@@ -1,3 +1,5 @@
+from flask import current_app
+
 from app import api_client
 from app.main import main
 from app.main.views import render_page
@@ -5,7 +7,9 @@ from app.main.views import render_page
 
 @main.route('/resources')
 def resources():
-    books = api_client.get_books()
+    books = None
+    if not current_app.config["SHOW_RESOURCE_MAINTENANCE"]:
+        books = api_client.get_books()
 
     return render_page(
         'views/resources.html',
