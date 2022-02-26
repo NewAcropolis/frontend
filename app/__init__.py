fa9d7201fa3b943797d42dd5494d2b42f50a7fbc
Ken Tsang <kenlt.uk@gmail.com>
2022-02-26 13:50:49 +0000

    d

--- app/__init__.py
+++ app/__init__.py
@@ -60,7 +60,7 @@ def create_app(**kwargs):
     setup_config(application, configs[environment_state])
 
     application.config.update(kwargs)
-    print("*** environment", os.environ['ENVIRONMENT'])
+    print("*** environment", os.environ.get('ENVIRONMENT', 'test'))
     print("*** testing", application.config['TESTING'])
     if not application.config['TESTING']:
         use_gaesession(application)
