1559e301881972af53a5936c6cda144896744135
kenlt-uk <30796865+kenlt-uk@users.noreply.github.com>
2022-09-16 16:53:06 +0100

    Update magazines start and end

--- app/main/views/magazines.py
+++ app/main/views/magazines.py
@@ -2,11 +2,15 @@ from app import api_client
 from app.main import main
 from app.main.views import render_page
 
+LOOK_AHEAD = 5
+
 
 @main.route('/magazines')
 @main.route('/magazines/<int:offset>')
 def magazines(offset=0):
     magazines = api_client.get_magazines()
+    start = offset
+    end = offset + LOOK_AHEAD
 
     return render_page(
         'views/magazines.html',
