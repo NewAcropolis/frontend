9ed53306b440c7f2c5b616179472481a86e8b76f
Ken Tsang <kenlt.uk@gmail.com>
2023-05-26 01:42:35 +0100

    Move magazinesV2.py to magazines.py

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -5,7 +5,7 @@ main = Blueprint('main', __name__)  # noqa
 from app.main.views import ( # noqa
     index, articles, api, cache, courses, download, events,
     magazines, member, order, queue, shop, subscription,
-    tag, magazinesV2
+    tag
 )
 
 from app.main.views.admin import (  # noqa
--- app/main/views/magazines.py
+++ app/main/views/magazines.py
@@ -1,4 +1,5 @@
 from app import api_client
+from app.config import use_sim_data
 from app.main import main
 from app.main.views import render_page
 
@@ -6,17 +7,24 @@ from app.main.views import render_page
 @main.route('/magazines')
 @main.route('/magazines/<int:page>')
 def magazines(page=0):
-    magazines = api_client.get_magazines()
+    if use_sim_data():
+        articles = api_client.get_articles_summary()
+        return render_page(
+            'views/magazinesV2.html',
+            articles=articles
+        )
+    else:
+        magazines = api_client.get_magazines()
 
-    start = page * 5
-    end = (page + 1) * 5
-    next_page = page + 1
+        start = page * 5
+        end = (page + 1) * 5
+        next_page = page + 1
 
-    if end > len(magazines):
-        next_page = None
+        if end > len(magazines):
+            next_page = None
 
-    return render_page(
-        'views/magazines.html',
-        magazines=magazines[start:end],
-        next_page=next_page
-    )
+        return render_page(
+            'views/magazines.html',
+            magazines=magazines[start:end],
+            next_page=next_page
+        )
