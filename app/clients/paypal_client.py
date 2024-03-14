ed1801716b19a359afb8c9595ee85d19e9a612ed
Ken Tsang <kenlt.uk@gmail.com>
2024-03-14 01:40:01 +0000

    Add paypal_client

---
+++ app/clients/paypal_client.py
@@ -0,0 +1,24 @@
+from flask import current_app, render_template
+
+from app.clients import BaseAPIClient
+
+
+class PaypalClient(BaseAPIClient):
+    def init_app(self, app, paypal_url):
+        super(PaypalClient, self).init_app(app, base_url=paypal_url, client_id=None)
+
+    def checkout(self, delivery_form, cart):
+        data = {
+            "first_name": delivery_form.first_name.data,
+            "last_name": delivery_form.last_name.data,
+            "address1": delivery_form.address1.data,
+            "address2": delivery_form.address2.data,
+            "city": delivery_form.city.data,
+            "zip": delivery_form.zip.data,
+        }
+
+        return render_template(
+            'views/checkout.html',
+            data=data,
+            cart=cart
+        )
